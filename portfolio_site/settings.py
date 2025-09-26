0dd1aaa45ee946eefd5c8d931fd83052753304a6
sudharshan <sudharshanmonith@gmail.com>
2025-09-26 18:23:30 +0530

    Fix STATIC_ROOT for Render

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -127,11 +127,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-import os
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JS, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ← Critical for Render
